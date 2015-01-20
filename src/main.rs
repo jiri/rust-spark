3c82bcdff4a13705b299d07f9e4cc2999ed44f57
Jiří Šebele <sindriava@gmail.com>
2015-01-20 20:40:28 +0100

    Tidy up the code and add opts

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,58 @@
 #![allow(unstable)]
 
 extern crate spark;
+extern crate getopts;
+
+use std::os;
+use getopts::optflag;
 
 /* TODO:
  *  - stdin support
  *  - show help
  *  - docs
  */
+
 fn main() {
-    let values: Vec<f32> = std::os::args().iter()
+    let args = os::args();
+
+    let opts = [
+        /* - Wait for EOF to draw */
+        /* - Set min / max values */
+        optflag("h", "help", "display this help text"),
+        optflag("v", "version", "output version information and exit"),
+    ];
+
+    let matches = match getopts::getopts(args.tail(), &opts) {
+        Ok(m) => m,
+        Err(f) => {
+            println!("{}", f);
+            os::set_exit_status(1);
+            return;
+        }
+    };
+
+    if matches.opt_present("v") {
+        println!("{}", env!("CARGO_PKG_VERSION"));
+        return;
+    }
+
+    if matches.opt_present("h") {
+        /* TODO: Print help here */
+        println!("{}", getopts::usage("Echo the STRING(s) to standard output.", &opts).as_slice());
+        return;
+    }
+
+    // println!("{:?}", args);
+
+    let values: Vec<f32> = os::args().iter()
         .skip(1)
-        .flat_map(|x| x.split(','))
-        .flat_map(|x| x.split(';'))
-        .filter(|x| x.len() != 0)
-        .map(|s| {
-            match s.parse::<f32>() {
-                Some(i) => i,
-                None    => { println!("{}", s); 0.0 },
-            }
-        })
+        .flat_map(|n| n.split(','))
+        .filter_map(|n| n.parse::<f32>())
         .collect();
 
-    println!("{}", spark::graph(values.as_slice()));
+    // println!("{:?}", values);
+
+    if !values.is_empty() {
+        println!("{}", spark::graph(values.as_slice()));
+    }
 }
