c47f61f61163dfb543986712c6efc72943f72e67
Jiří Šebele <sindriava@gmail.com>
2015-01-20 13:53:43 +0100

    Add a TODO comment

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 extern crate spark;
 
+/* TODO:
+ *  - stdin support
+ *  - better argument parsing
+ *  - tests
+ */
 fn main() {
     // println!("{}", spark::graph(&[1.0, 5.0, 22.0, 13.0, 53.0]));
     /* Should look like ▁▁▃▂█ */
