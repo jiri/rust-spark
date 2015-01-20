48d0ebd8077d5212e9f84c8432eb1fbfa37480be
Jiří Šebele <sindriava@gmail.com>
2015-01-20 04:46:17 +0100

    A very simple initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
-pub fn hello() {
-    println!("Hello, world!");
+use std::f32::MAX_VALUE as f32_max;
+use std::num::Float;
+
+pub fn graph(args: &[f32]) -> String {
+    let ticks = ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"];
+    let mut sparkline = String::with_capacity(args.len());
+
+    let mut min: f32 = f32_max;
+    let mut max: f32 = 0.0;
+
+    for &i in args.iter() {
+        if i > max {
+            max = i;
+        }
+        if i < min {
+            min = i;
+        }
+    }
+
+    let ratio = (ticks.len() - 1) as f32 / (max - min);
+
+    for &i in args.iter() {
+        sparkline.push_str(ticks[((i - min) * ratio).floor() as usize]);
+    }
+
+    sparkline
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate spark;
 
 fn main() {
-    spark::hello();
+    println!("{}", spark::graph(&[1.0, 5.0, 22.0, 13.0, 53.0]));
+    /* Should look like ▁▁▃▂█ */
 }
