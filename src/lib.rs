b808440b18a8157e3d0bcff2825bb053fec80d62
Jiří Šebele <sindriava@gmail.com>
2015-01-23 00:58:48 +0100

    Add doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,26 @@
 use std::f32::MAX_VALUE as f32_max;
 use std::num::Float;
 
-pub fn graph(args: &[f32]) -> String {
+/// `graph` generates a string representation of the values as a sparkline.
+///
+/// # Arguments
+///
+/// * `values` - The values to graph.
+///
+/// # Example
+///
+/// ```rust
+/// let sparkline = spark::graph(&[1.0, 5.0, 22.0, 13.0, 53.0]);
+/// assert_eq!(sparkline, "▁▁▃▂█");
+/// ```
+pub fn graph(values: &[f32]) -> String {
     let ticks = "▁▂▃▄▅▆▇█";
 
     /* XXX: This doesn't feel like idiomatic Rust */
     let mut min: f32 = f32_max;
     let mut max: f32 = 0.0;
 
-    for &i in args.iter() {
+    for &i in values.iter() {
         if i > max {
             max = i;
         }
@@ -25,7 +37,7 @@ pub fn graph(args: &[f32]) -> String {
         (ticks.chars().count() - 1) as f32 / (max - min)
     };
 
-    args.iter()
+    values.iter()
         .cloned()
         .map(|n| (n - min) * ratio)
         .map(|n| n.floor() as usize)
